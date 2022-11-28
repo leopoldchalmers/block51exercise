4d9e33c12353375cb5750edba142100029dd376e
leopoldchalmers <112471595+leopoldchalmers@users.noreply.github.com>
2022-11-28 14:54:13 +0100

    update

---
+++ src/polygons/PolygonFactory.java
@@ -0,0 +1,10 @@
+package polygons;
+import polygons.polygon.Polygon;
+import javax.swing.*;
+import java.awt.Graphics;
+import java.util.*;
+
+
+public class PolygonFactory extends JComponent{
+    private List<Polygon> polygons;
+}
--- src/polygons/polygon/Rectangle.java
+++ src/polygons/polygon/Rectangle.java
@@ -9,7 +9,7 @@ public class Rectangle extends Polygon {
     public Rectangle(int x, int y) {
         super(x,y);
     }
-
+    @Override
     public void paint(Graphics g){
         g.drawRect(getCenterPoint().x -20, getCenterPoint().y -10, 40, 20);
     }
